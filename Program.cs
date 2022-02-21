8c02efaa7741009faa568a8229829afedf22f20a
denamo <artstarxd@outlook.com>
2022-02-21 11:43:35 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Sharp11
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Account account1 = new Account();
+            Account account2 = new Account();
+            Account account3 = new Account();
+
+            account3.FillTypeAccount(TypeOfAccount.Credit);
+            account1.FillBalance(100000);
+            account2.FillNumber(4);
+
+            account1.FullInfo();
+            account2.FullInfo();
+            account3.FullInfo();
+        }
+    }
+}
---
+++ Tasks.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Sharp11
+{
+    public enum TypeOfAccount {Debit, Credit}
+
+    public class Account
+    {
+        private int _number;
+
+        private static int _counter = 1;
+
+        private decimal _balance;
+
+        private TypeOfAccount _typeAccount;
+
+        public Account()
+        {
+            _number = Increase();
+        }
+
+        private static int Increase()
+        {
+            return _counter++;
+        }
+
+        public void FillNumber(int number)
+        {
+            _number = number;
+        }
+
+        public void FillBalance(decimal balance)
+        {
+            _balance = balance;
+        }
+
+        public void FillTypeAccount(TypeOfAccount typeAccount)
+        {
+            _typeAccount = typeAccount;
+        }
+
+        public void FullInfo()
+        {
+            Console.WriteLine($"Type Of Account: {_typeAccount}, Number of Account: {_number}, Balance: {_balance}");
+        }
+    }
+}
